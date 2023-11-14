59095389dac0f968dacf8e76933d34d1cc219a7b
Marek Kandulski <marek.kandulski@proton.me>
2023-11-14 22:57:47 +0100

    moved set_listeners() and setup function to main.js

--- src/script/main.js
+++ src/script/main.js
@@ -1,11 +1,39 @@
-const data = 
-{
-    max_short_name_length: 2,
-    refresh_value_frame_delay: 50,
-    refresh_value_frame_rate: 20,
-}
-
 function get_id(id)
 {
     return document.getElementById(id)
 }
+
+function set_listeners(listeners_data)
+{
+    for(elem of listeners_data)
+    {
+        for(listener of elem.events)
+           get_id(elem.id).addEventListener(listener.type, listener.handler)
+    }    
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    create_mode_list()
+    set_mode(data.default_mode)
+    document.addEventListener("click", handleDocumentClick)
+    set_listeners([
+        {id: "input", events: [
+            {type: "click", handler: handleOuterInputClick},
+        ]},
+        {id: "input-text", events: [
+            {type: "focus", handler: handleInnerInputFocus},
+            {type: "blur", handler: handleInnerInputBlur},
+            {type: "keydown", handler: handleInnerInputKeyDown},
+            {type: "input", handler: handleInnerInputInput},
+        ]},
+        {id: "change-mode-btn", events: [
+            {type: "click", handler: handleChangeModeBtnClick},
+        ]},
+        {id: "mode-list", events: [
+            {type: "keydown", handler: handleModeListKeyDown},
+        ]},
+        {id: "calculate-btn", events: [
+            {type: "click", handler: handleCalculateBtnClick},
+        ]},
+    ])
+})
