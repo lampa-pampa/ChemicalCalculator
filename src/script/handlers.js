b8214da7b5a342c7c6cf15e7c92fd4f2a7c4d199
Marek Kandulski <marek.kandulski@proton.me>
2023-11-14 22:56:36 +0100

    renamed event_listeners.js to handlers.js (due to set_listeners transfer)

---
+++ src/script/handlers.js
@@ -0,0 +1,67 @@
+function handleDocumentClick(e)
+{
+    const music = new Audio("src/sounds/music.mp3")
+    music.loop = true
+    // music.play()
+    document.removeEventListener("click", handleDocumentClick)
+}
+
+function handleOuterInputClick(e)
+{
+    e.target.children[0].focus()
+}
+
+function handleInnerInputFocus(e)
+{
+    e.target.parentNode.classList.add("active")
+}
+
+function handleInnerInputBlur(e)
+{
+    e.target.parentNode.classList.remove("active")
+}
+
+function handleInnerInputKeyDown(e)
+{
+    states.cur_cursor_index = get_cursor_index(e.target)
+    states.cur_pressed_key = e.key
+    if(states.cur_pressed_key == "Enter")
+    {
+        get_id("calculate-btn").click()
+        e.target.blur()
+    }
+}
+
+function handleInnerInputInput(e)
+{
+    refresh_input_value(e.target)
+    refresh_cursor(e.target)
+}
+
+function handleChangeModeBtnClick(e)
+{
+    get_id("mode-list").focus()
+}
+
+function handleModeListKeyDown(e)
+{
+    if(e.key == "Escape")
+        e.target.blur()
+}
+
+function handleCalculateBtnClick(e)
+{
+    const value = get_id("input-text").textContent
+    if(validate_input_value(value))
+    {
+        if(value)
+        {
+            const compound = parse_input_value(get_id("input-text").textContent)
+            boom()
+        }
+    }
+    else
+    {
+        throw new Error("pal gume")
+    }
+}
