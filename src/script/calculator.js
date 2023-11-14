2d843fd41258dec7b8c03300d2b75d36fa9af4a5
Marek Kandulski <marek.kandulski@proton.me>
2023-11-14 23:21:18 +0100

    added calculator.js

---
+++ src/script/calculator.js
@@ -0,0 +1,22 @@
+class Calculator
+{
+    static calculate_percentage_composition(compound)
+    {
+        alert("percentage")
+    }
+
+    static calculate_empirical_formula(compound)
+    {
+        alert("empirical_formula")
+    }
+
+    static calculate_group_formula(compound)
+    {
+        alert("group_formula")
+    }
+
+    static calculate_structural_formula(compound)
+    {
+        alert("structural_formula")
+    }
+}
