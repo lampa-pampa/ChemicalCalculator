a5f4e9124f5c735e8a61769a816188d2a87c53c0
Marek Kandulski <marek.kandulski@proton.me>
2023-11-15 22:45:30 +0100

    changed alerts to console.log-s in calculator to test animations

--- src/script/calculator.js
+++ src/script/calculator.js
@@ -2,21 +2,24 @@ class Calculator
 {
     static calculate_percentage_composition(compound)
     {
-        alert("percentage")
+        for(const element of compound.elements)
+        {
+            
+        }
     }
 
     static calculate_empirical_formula(compound)
     {
-        alert("empirical_formula")
+        console.log("empirical_formula")
     }
 
     static calculate_group_formula(compound)
     {
-        alert("group_formula")
+        console.log("group_formula")
     }
 
     static calculate_structural_formula(compound)
     {
-        alert("structural_formula")
+        console.log("structural_formula")
     }
 }
