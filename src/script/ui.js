09b3edf0b1ee8df51a10f326b7a47934d73689e5
Marek Kandulski <marek.kandulski@proton.me>
2023-11-15 08:17:48 +0100

    moved all funcions form animations.js to ui.js deleted animations.js

--- src/script/ui.js
+++ src/script/ui.js
@@ -1,4 +1,37 @@
 class UI
 {
-    
+    static run_animation(node, animation_name)
+    {
+        node.style.animationName = ""
+        node.offsetWidth
+        node.style.animationName = animation_name
+    }
+
+    static boom()
+    {
+        UI.run_animation(get_id("input"), "boom-input")
+        UI.run_animation(get_id("output"), "fade-in-output") 
+        const music = new Audio("src/sounds/boom.mp3")
+        music.play()
+    }
+
+    static smooth_value_refresh(node, value)
+    {
+        node.textContent = 0
+        let diff = 1
+        if(value > data.refresh_value_frame_rate)
+            diff = Math.floor(value / data.refresh_value_frame_rate)
+        const interval = setInterval(() => {
+            const cur_value = parseInt(node.textContent)
+            if(cur_value < value)
+            {
+                if(value - cur_value >= diff)
+                    node.textContent = cur_value + diff
+                else
+                    node.textContent = value
+            }
+            else
+                clearInterval(interval)   
+        }, data.refresh_value_frame_delay)
+    }
 }
