468054fa05e079d42e76c7d0b73d237bda97333d
Marek Kandulski <marek.kandulski@proton.me>
2023-11-16 20:05:21 +0100

    moved output methods to separate class

--- src/script/main_ui.js
+++ src/script/main_ui.js
@@ -15,7 +15,6 @@ class MainUi
 
     play_music()
     {
-        music = true
         this.music.volume = data.sounds.music.start_volume
         this.music.play()
         const self = this
@@ -36,36 +35,5 @@ class MainUi
     {
         const sound = new Audio(data.sounds.fx.dir + data.sounds.fx.names[sound_name])
         sound.play()
-    }
-
-    smooth_value_refresh(node, value)
-    {
-        node.textContent = 0
-        let diff = 1
-        if(value > data.refresh_value_frame_rate)
-            diff = Math.floor(value / data.refresh_value_frame_rate)
-        const interval = setInterval(() => {
-            const cur_value = parseInt(node.textContent)
-            if(cur_value < value)
-            {
-                if(value - cur_value >= diff)
-                    node.textContent = cur_value + diff
-                else
-                    node.textContent = value
-            }
-            else
-                clearInterval(interval)   
-        }, data.refresh_value_frame_delay)
-    }
-
-    output_write(child_node)
-    {
-        get_id("output").innerHTML = ""
-        this.output_append(child_node)
-    }
-
-    output_append(child_node)
-    {
-        get_id("output").appendChild(child_node)
     }
 }
