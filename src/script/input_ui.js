a4eb931759fdd4a761280560a9db461717985724
Marek Kandulski <marek.kandulski@proton.me>
2023-11-16 23:03:57 +0100

    moved alert() from input_ui.js to output_ui.js

--- src/script/input_ui.js
+++ src/script/input_ui.js
@@ -1,10 +1,4 @@
 class InputUI
 {
-    alert(message)
-    {
-        const alert = document.createElement("span")
-        alert.classList.add("alert")
-        alert.textContent = message
-        output_ui.write(alert)
-    }
+    
 }
